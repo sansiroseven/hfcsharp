017087cc127fc755c7a9d0cf9c8c0139a6235698
michaelxiaomt <michaelxiaomt@gmail.com>
2021-07-24 12:32:04 +0800

    Add basic intro update

---
+++ chapter02/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+
+            int heigth = 2 + 4;
+            int area = width * heigth;
+
+
+            while(area < 50)
+            {
+                heigth++;
+                area = heigth * width;
+            }
+
+
+            Console.WriteLine(area);
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            //bool trunValue = true;
+            //Console.WriteLine(trunValue);
+
+            //int count = 5;
+            //int i = 0;
+            //while (count > 0)
+            //{
+            //    count = count * 3;
+            //    count = count * -1;
+            //    i++;
+            //}
+
+
+
+            //Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
